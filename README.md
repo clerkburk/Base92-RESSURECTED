4b4e9db723a924ce06f5d4bd035b2e42393a0a71
clerkburk <profspinneps10@gmail.com>
2026-05-19 11:57:15 +0200

    lige5n7gfeb 987trsg4789tgs0ns9t7

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# Base92 JavaScript Encoder and Decoder
+# Base92 TypeScript Encoder and Decoder
 
 > GREAT = Base92, GOOD = Base64;
 
-> Probably THE ONLY PURE-JS BASE92 (2023) encoding/decoding system in javascript, most notably also faster than half of the st*pid base64 encoding/decoding library (it re-use the memory system and is written in low-level javascript in order to make the engine work based on lighter dataset and operations)
+> Probably THE ONLY PURE-JS BASE92 (2026) encoding/decoding system in javascript, most notably also faster than half of the st*pid base64 encoding/decoding library (it re-use the memory system and is written in low-level javascript in order to make the engine work based on lighter dataset and operations)
 
 > While Gzip (used in most web servers nowadays) is well suited for Base64, it doesn't lose any key (compression) performance based on Base92, but the greatest of "it all" is that, IT SHOULD ALWAYS HAVE BEEN BASE92 IN JSON AND SUCH BECAUSE IT CAN BE EASILY (The lazy situp-wordu of many companies are just operator not architect in "Automated Information Technology" Knowledge's Body of knowledge)
 
@@ -14,7 +14,7 @@
 
 The `Base92` JavaScript module provides methods to encode and decode data using a base92+64 encoding scheme. It is not faster than base64 but it provides a more space-efficient result in UTF-8 or UTF-16 source code strings, making it useful for all kinds of inline data in source code.
 
-The module is minified and has a footprint of just 14 kB, and requires no dependencies. This makes it an easy and lightweight solution for your encoding needs.
+The module is minified and has a footprint of just 4 kB, and requires no dependencies. This makes it an easy and lightweight solution for your encoding needs.
 
 ## Installation
 
@@ -26,50 +26,58 @@ npm install base92
 
 ## Usage
 
-Import the module into your JavaScript code:
+Import the module into your ESM JavaScript/TypeScript code:
 
-```javascript
-var BaseN = require('base92');
-var Base92 = BaseN.Base92;
-var Base64 = BaseN.Base64;
+```ts
+import { Base92, Base64 } from "base92"
+// or
+import * as bs92 from "base92"
+const base92 = new bs92.Base92()
+const base64 = new bs92.Base64()
 ```
 
-Import the minified file into your JavaScript code:
+If you want to use the browser bundle directly:
 
-```javascript
- // <script> 'index.min.js';
-var Base92 = window.Base92;
-var Base64 = window.Base64;
+```html
+<script src="https://cdn.jsdelivr.net/npm/base92/dist/index.min.js"></script>
+<script>
+const base92 = new Base92Codec.Base92()
+const base64 = new Base92Codec.Base64()
+</script>
 ```
 
 ### Encoding
 
-To encode data, create an instance of the `Base92` class and use the encode method. This method takes a Uint8Array` as input:
+To encode data, create an instance of the `Base92` class and use the encode method. This method takes a `Uint8Array` as input:
 
-```javascript
-var data = new Uint8Array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
-var base92 = new Base92();
-var base64 = new Base64();
+```ts
+const data = new Uint8Array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+// for string data
+const data = new TextEncoder().encode("I'm in agony")
+// works with files and such (a Buffer inherits from Uint8Array)
+const data = fs.readFileSync("path/to/file")
+const base92 = new Base92()
+const base64 = new Base64()
 
 // You have to tell if you want to have a new Uint8Array because... Wait!
 // If you don't the system will reuse the memory system of the instanciated class' JS object, OK?!
 // Optional, WILL trigger a copy (if set to true) of the internal (memory) storage system...
-var slice = false; // This variable' parameter's (secondary) position is not mandatory
+const slice = false // This variable' parameter's (secondary) position is not mandatory
 // Just to know, if you don't get a copy and do encoding operation twice, you might have the same Uint8Array twice
 
-var encodedData92 = base92.encode(data, slice); // "  ,@%7 sBv&]1"
-var encodedData64 = base64.encode(data, slice); // "AAECAwQFBgcICQ=="
-console.log(encodedData92, encodedData64);
+const encodedData92 = base92.encode(data, slice) // "  ,@%7 sBv&]1"
+const encodedData64 = base64.encode(data) // "AAECAwQFBgcICQ=="
+console.log(encodedData92, encodedData64)
 ```
 
 ### Decoding
 
-To `decode data, use the decode method of the `Base92` class instance. This method takes a string as input:
+To decode data, use the decode method of the `Base92` class instance. This method takes a string as input:
 
-```javascript
-var decodedData92 = base92.decode(encodedData92);
-var decodedData64 = base64.decode(encodedData64);
-console.log(decodedData92, decodedData64);
+```ts
+const decodedData92 = base92.decode(encodedData92)
+const decodedData64 = base64.decode(encodedData64)
+console.log(decodedData92, decodedData64)
 ```
 
 ## License (before)
@@ -78,6 +86,8 @@ THE BEERWARE LICENSE (Revision 42):
 
 ## License (now)
 
-COPYLEFT "666" SAIL HATAN (Better get human-right(s) before any copyright)
+COPYLEFT "666" SAIL HATAN (Better get human-right(s) before any copyright <!-- yessirrr -->)
 
 HELL WAY SO FAAAAST --> JAVASCRIPT SYSTEM! (NOT AFRAID TO TELL YOU TO TEST PERF.)
+
+> TAKING A SHOWER IS THE ENEMY OF RUST-DEVS
---
+++ index.ts
@@ -0,0 +1,210 @@
+// This is a fan-made implementation of Base92 encoding and decoding in TypeScript.
+// While I'm not 100% certain what the 666-License allows, I hope that this allows me to fork this code
+
+const DEFAULT_MEMORY_LENGTH = 8192 * 2
+
+const TEXT_ENCODER = new TextEncoder()
+const TEXT_DECODER = new TextDecoder()
+
+const BASE64_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+const BASE64_ALPHABET_CODES = TEXT_ENCODER.encode(BASE64_ALPHABET)
+const BASE64_DECODE_TABLE = new Uint8Array(256).fill(255)
+for (let i = 0; i < BASE64_ALPHABET_CODES.length; i += 1)
+	BASE64_DECODE_TABLE[BASE64_ALPHABET_CODES[i]] = i & 0xff
+
+const BASE92_MAPPING = [
+	32, 33, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
+	51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68,
+	69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86,
+	87, 88, 89, 90, 91, 93, 94, 95, 97, 98, 99, 100, 101, 102, 103, 104, 105,
+	106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120,
+	121, 122, 123, 124, 125, 126,
+]
+const BASE92_DECODE_TABLE = new Uint8Array(256).fill(255)
+for (let i = 0; i < BASE92_MAPPING.length; i += 1)
+	BASE92_DECODE_TABLE[BASE92_MAPPING[i]] = i & 0xff
+
+class ReusableMemory {
+	protected memory: Uint8Array
+	private readonly memoryLength: number
+
+	constructor(memoryLength = DEFAULT_MEMORY_LENGTH) {
+		this.memoryLength = memoryLength;
+		this.memory = new Uint8Array(memoryLength)
+	}
+
+	protected ensureMemory(size: number) {
+		if (size <= this.memory.length)
+			return
+		this.memory = new Uint8Array(size + (size & this.memoryLength))
+	}
+
+	protected view(length: number, copy = false): Uint8Array {
+		return copy ? this.memory.slice(0, length) : this.memory.subarray(0, length)
+	}
+}
+
+export class Base64 extends ReusableMemory {
+	encode(input: Uint8Array): string {
+		const outputLength = Math.ceil(input.length / 3) * 4
+		this.ensureMemory(outputLength)
+
+		let i = 0
+		let j = 0
+
+		while (i + 2 < input.length) {
+			const a = input[i++];
+			const b = input[i++];
+			const c = input[i++];
+
+			this.memory[j++] = BASE64_ALPHABET_CODES[a >> 2]
+			this.memory[j++] = BASE64_ALPHABET_CODES[((a & 0x03) << 4) | (b >> 4)]
+			this.memory[j++] = BASE64_ALPHABET_CODES[((b & 0x0f) << 2) | (c >> 6)]
+			this.memory[j++] = BASE64_ALPHABET_CODES[c & 0x3f]
+		}
+
+		if (i < input.length) {
+			const a = input[i++]
+			const b = i < input.length ? input[i] : 0
+
+			this.memory[j++] = BASE64_ALPHABET_CODES[a >> 2]
+			this.memory[j++] = BASE64_ALPHABET_CODES[((a & 0x03) << 4) | (b >> 4)]
+
+			if (i < input.length)
+				this.memory[j++] = BASE64_ALPHABET_CODES[(b & 0x0f) << 2]
+			else
+				this.memory[j++] = 61
+			this.memory[j++] = 61
+		}
+
+		return TEXT_DECODER.decode(this.memory.subarray(0, j))
+	}
+
+	decode(input: string, copyMemory = false): Uint8Array {
+		if (input.length % 4 !== 0)
+			throw new Error("Unable to parse base64 string.")
+
+		const equalsIndex = input.indexOf("=");
+		if (equalsIndex !== -1 && equalsIndex < input.length - 2)
+			throw new Error("Unable to parse base64 string.")
+
+		const missingOctets = input.endsWith("==") ? 2 : input.endsWith("=") ? 1 : 0
+		const outputLength = (3 * (input.length / 4)) | 0
+		this.ensureMemory(outputLength)
+
+		let j = 0;
+		for (let i = 0; i < input.length; i += 4) {
+			const c0 = input.charCodeAt(i)
+			const c1 = input.charCodeAt(i + 1)
+			const c2 = input.charCodeAt(i + 2)
+			const c3 = input.charCodeAt(i + 3)
+
+			const a = BASE64_DECODE_TABLE[c0]
+			const b = BASE64_DECODE_TABLE[c1]
+			const c = c2 === 61 ? 0 : BASE64_DECODE_TABLE[c2]
+			const d = c3 === 61 ? 0 : BASE64_DECODE_TABLE[c3]
+
+			if (a === 255 || b === 255 || (c2 !== 61 && c === 255) || (c3 !== 61 && d === 255))
+				throw new Error("Unable to parse base64 string.")
+
+			const block = (a << 18) | (b << 12) | (c << 6) | d;
+			this.memory[j++] = (block >> 16) & 0xff
+			this.memory[j++] = (block >> 8) & 0xff
+			this.memory[j++] = block & 0xff
+		}
+
+		return this.view(outputLength - missingOctets, copyMemory)
+	}
+}
+
+export class Base92 extends ReusableMemory {
+	private static readonly TILDE = 126
+
+	encode(input: Uint8Array): string {
+		if (input.length === 0) {
+			this.ensureMemory(1)
+			this.memory[0] = Base92.TILDE
+			return TEXT_DECODER.decode(this.memory.subarray(0, 1))
+		}
+
+		const remainderBits = (input.length * 8) % 13
+		const outputLength =
+			remainderBits === 0
+				? (2 * ((input.length * 8) / 13)) | 0
+				: remainderBits < 7
+					? (2 * ((input.length * 8) / 13) + 1) | 0
+					: (2 * ((input.length * 8) / 13) + 2) | 0
+		this.ensureMemory(outputLength)
+
+		let workspace = 0
+		let wssize = 0
+		let j = 0
+
+		for (let i = 0; i < input.length; i += 1) {
+			workspace = ((workspace << 8) | input[i]) >>> 0
+			wssize += 8
+			if (wssize >= 13) {
+				wssize -= 13
+				const value = (workspace >> wssize) & 8191
+				this.memory[j++] = BASE92_MAPPING[(value / 91) | 0]
+				this.memory[j++] = BASE92_MAPPING[value % 91]
+			}
+		}
+
+		if (wssize > 0 && wssize < 7) {
+			const value = (workspace << (6 - wssize)) & 63
+			this.memory[j++] = BASE92_MAPPING[value]
+		} else if (wssize >= 7) {
+			const value = (workspace << (13 - wssize)) & 8191
+			this.memory[j++] = BASE92_MAPPING[(value / 91) | 0]
+			this.memory[j++] = BASE92_MAPPING[value % 91]
+		}
+
+		return TEXT_DECODER.decode(this.memory.subarray(0, j))
+	}
+
+	decode(input: string, copyMemory = false): Uint8Array {
+		if (input.length === 0 || input.charCodeAt(0) === Base92.TILDE)
+			return Uint8Array.of(Base92.TILDE)
+
+		if (input.length < 2)
+			return new Uint8Array(0)
+
+		const outputLength = ((input.length * 13 + (input.length % 2) * 6) / 8) | 0
+		this.ensureMemory(outputLength)
+
+		let workspace = 0
+		let wssize = 0
+		let j = 0
+
+		for (let i = 0; i + 1 < input.length; i += 2) {
+			const a = BASE92_DECODE_TABLE[input.charCodeAt(i) & 0xff]
+			const b = BASE92_DECODE_TABLE[input.charCodeAt(i + 1) & 0xff]
+			if (a === 255 || b === 255)
+				throw new Error("Unable to parse base92 string.")
+
+			workspace = ((workspace << 13) | (Math.imul(a, 91) + b)) >>> 0
+			wssize += 13
+
+			while (wssize >= 8) {
+				wssize -= 8
+				this.memory[j++] = (workspace >> wssize) & 0xff
+			}
+		}
+
+		if (input.length % 2 === 1) {
+			const value = BASE92_DECODE_TABLE[input.charCodeAt(input.length - 1) & 0xff]
+			if (value === 255)
+				throw new Error("Unable to parse base92 string.")
+
+			workspace = ((workspace << 6) | value) >>> 0
+			wssize += 6
+			while (wssize >= 8) {
+				wssize -= 8
+				this.memory[j++] = (workspace >> wssize) & 0xff
+			}
+		}
+
+		return this.view(j, copyMemory)
+	}
+}
